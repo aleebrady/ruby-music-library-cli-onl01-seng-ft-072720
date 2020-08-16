d245daf19699776cd2b4a1bc1d19b88f8e4226a6
Aaron Brady <aleebrady@gmail.com>
2020-08-16 17:58:39 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -18,6 +18,7 @@ class MusicLibraryController
       puts "To play a song, enter 'play song'."
       puts "To quit, type 'exit'."
       puts "What would you like to do?"
+    end
   end
   
   def list_songs
