4b2cc8eabfd3dad2d04a2b8e7f732c1736f73d12
Aaron Brady <aleebrady@gmail.com>
2020-08-16 17:52:47 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -7,8 +7,8 @@ class MusicLibraryController
   end
   
   def call
-    input = nil
-    while input != "exit" do
+    input = gets.strip
+    if input != "exit"
       puts "Welcome to your music library!"
       puts "To list all of your songs, enter 'list songs'."
       puts "To list all of the artists in your library, enter 'list artists'."
