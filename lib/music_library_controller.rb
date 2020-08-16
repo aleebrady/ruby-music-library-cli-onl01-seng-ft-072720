b029c54c580423ee3e66971a20ef18591bb89941
Aaron Brady <aleebrady@gmail.com>
2020-08-16 17:54:19 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -18,13 +18,6 @@ class MusicLibraryController
       puts "To play a song, enter 'play song'."
       puts "To quit, type 'exit'."
       puts "What would you like to do?"
-      
-      case input
-      when "list_songs"
-        list_songs
-        
-      end
-      
     end
   end
   
